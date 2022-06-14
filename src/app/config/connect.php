a0f93877b01c677e6c0b80d937b262bd943a053e
LuizFernandoDondici <luizfernando.dondici@outlook.com>
2022-06-14 19:10:28 -0300

    create: relationship, fixed: var-names in connect

--- src/app/config/connect.php
+++ src/app/config/connect.php
@@ -14,17 +14,19 @@ class Connect
             $pdo = new PDO('sqlite:' . __DIR__ . '../../database/galeria-fotos.sqlite');
 
             $queryGaleria = 
-                'CREATE TABLE IF NOT EXISTS galeria(
-                    id_img INTEGER PRIMARY KEY,
-                    path_img TEXT,
-                    name_img TEXT
+                'CREATE TABLE IF NOT EXISTS photo(
+                    id_photo INTEGER PRIMARY KEY,
+                    path_photo TEXT,
+                    name_photo TEXT,
+                    id_user INTEGER,
+                    FOREIGN KEY (id_user) REFERENCES login (id_user)
                 );';
 
             $queryLogin = 
-                'CREATE TABLE IF NOT EXISTS login(
-                    id_login INTEGER PRIMARY KEY,
-                    email_login TEXT,
-                    pass_login TEXT
+                'CREATE TABLE IF NOT EXISTS user(
+                    id_user INTEGER PRIMARY KEY,
+                    email_user TEXT,
+                    pass_user TEXT
                 );';
             
             $pdo->exec($queryGaleria);
