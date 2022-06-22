818a5171984ae5acf61c5a609fcd137b30a52049
luizphicompany <luizfernando.dondici@outlook.com>
2022-06-22 20:43:42 -0300

    fixed: files

--- src/public/view/head/head.php
+++ src/public/view/head/head.php
@@ -8,8 +8,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../../public/style/body/body.css">
+    <link rel="stylesheet" href="../../../public/style/menu/menu.css">
     <link rel="stylesheet" href="../../../public/style/form-login/form-login.css">
     <link rel="stylesheet" href="../../../public/style/form-img/form-img.css">
+    <link rel="stylesheet" href="../../../public/style/alert/alert.css">
     <link rel="stylesheet" href="../../../public/style/error/error.css">
 
     <title> Galeria de Imagens </title>
